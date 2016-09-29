f209992d8e058d06d15bd268f4c11732bcc0856e
Adrian <adrianasdfs@gmail.com>
2016-09-29 11:22:58 +0800

    implemented a coroutine called when receive data from server

--- Assets/scripts/Game.cs
+++ Assets/scripts/Game.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
+
+using Newtonsoft.Json.Linq;
 
 public class Game : MonoBehaviour {
 
@@ -8,45 +11,111 @@ public class Game : MonoBehaviour {
 	// public variables
 
 	public GameObject gameTable;
-	public GameObject userAction;
 	public GameObject Overlay;
+	public GameObject round;
+
+	private Table _table;
+	private Text _roundTextComponent;
+	private OverlayScript _overlayscriptComponent;
+	private bool _hasReceivedData;
+	private string _responseType;
+	private JToken _responseData;
+
+
+
+
+	private SocketConnection _socket;
+
+	void Awake(){
+
+
+
+
+
+
+		StartCoroutine ("receiveDataCoroutine");
+	}
+
+	void changeRound(string roundInt){
+		string[] g = _roundTextComponent.text.Split (' ');
+		_roundTextComponent.text = g [0] + " " + roundInt;
+	}
+
+
+	IEnumerator receiveDataCoroutine(){
+
+		while (true) {
+
+			while (!_hasReceivedData) {
+				yield return null;
+			}
+
+
+			if (_responseType == "greet") {
+
+				changeRound (_responseData ["round"].ToString ());
+				_table.addOwnerId (_responseData ["user"].ToString ());
+
+				yield return new WaitForSeconds (0.6f);
+				_overlayscriptComponent.toggleLoadingText ();
+
+//				
+
+				_overlayscriptComponent.toggleWaitingText ();
+
+			} else if (_responseType == "move") {
+
+		
+			} else if (_responseType == "user") {
+
+
+			} else if (_responseType == "round") {
+
+				changeRound (_responseData ["round"].ToString ());
+
+			}
+
+			_hasReceivedData = false;
+		}
+
+	}
+
+	public void receiveData(string dt){
+
+		var objects = JArray.Parse (dt);
+
+		JToken response = objects.First["response"];
+		_responseType = (response ["type"]).ToString ();
+		_hasReceivedData = true;
+		_responseData = response ["data"];
+
+	}
 
 	void Start () {
 
-//		Overlay.SetActive (true);
-//
-//		Overlay.GetComponent<OverlayScript>().toggleLoadingText();
-//
-		Table t = gameTable.GetComponent<Table> ();
+		// Initialize private variables
+
+		_hasReceivedData = false;
+		_roundTextComponent = round.GetComponent<Text> ();
+		_table = gameTable.GetComponent<Table> ();
+		_overlayscriptComponent = Overlay.GetComponent<OverlayScript> ();
+
+
+		// Initialise websocket
+
+		_socket = new SocketConnection ();
+		_socket.setDelegate (this);
+		_socket.greet ();
+
+
+		// show loading text
+
+		_overlayscriptComponent.toggle();
+		_overlayscriptComponent.toggleLoadingText ();
 	}
 
 	void Update () {
 
 
 	}
-
-	/*
-	 * 
-	 * OnClick handler for pause button
-	 * 
-	 */
-
-	public void pauseButton(){
-		Overlay.SetActive (true);
-		Overlay.GetComponent<OverlayScript> ().showPause ();
-
-	}
-
-	/*
-	 * 
-	 * OnClick handler for info button
-	 * 
-	 */
-	 
-	public void infoButton(){
-		Overlay.SetActive (true);
-		Overlay.GetComponent<OverlayScript> ().showInfo ();
-	}
-
-
 }
