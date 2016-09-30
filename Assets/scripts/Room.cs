eae0b7bb479022c8e1baea472dfe4635868c0d23
Adrian <adrianasdfs@gmail.com>
2016-10-01 02:27:45 +0800

    script for the room list item

---
+++ Assets/scripts/Room.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Room : MonoBehaviour {
+
+	public GameObject roomId;
+	public GameObject roomSeq;
+	public GameObject numOfPlayer;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+
+}
