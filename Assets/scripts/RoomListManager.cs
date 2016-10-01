ef1b914479eab330ffeba9e8620f980dcb55a331
Adrian <adrianasdfs@gmail.com>
2016-10-01 14:49:31 +0800

    roomlist manager

---
+++ Assets/scripts/RoomListManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json.Linq;
+
+public class RoomListManager : MonoBehaviour,SocketConnectionInterface {
+
+	public GameObject spinner;
+	public GameObject j;
+	private SocketConnection _sock;
+
+
+	// Use this for initialization
+	void Start () {
+		spinner.SetActive (true);
+
+		_sock = new SocketConnection ();
+		_sock.setDelegate (this);
+//		_sock.getRoom ();
+
+	}
+
+	public void receiveData(string dt){
+//		Debug.Log (dt);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
