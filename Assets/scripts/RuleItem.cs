d16057acd12b9a196c6895461d014d70a5815c48
Adrian <adrianasdfs@gmail.com>
2016-10-05 01:36:04 +0800

    add getters for Id & toggle state

--- Assets/scripts/RuleItem.cs
+++ Assets/scripts/RuleItem.cs
@@ -6,8 +6,14 @@ public class RuleItem : MonoBehaviour {
 
 	public GameObject ruleName;
 	public GameObject ruleDescription;
+	public Toggle toggle;
 
 	private string _ruleId;
+
+	void Start(){
+		GetComponent<RectTransform> ().localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+
+	}
 
 	public void addDetails(string name,string description,string id){
 	
@@ -15,6 +21,16 @@ public class RuleItem : MonoBehaviour {
 		ruleDescription.GetComponent<Text> ().text = description;
 		_ruleId = id;
 
+	}
+
+	public bool isToggleOn(){
+
+		return toggle.isOn;
+
+	}
+
+	public string getId(){
+		return _ruleId;
 	}
 
 }
