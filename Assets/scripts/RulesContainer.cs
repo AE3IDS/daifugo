f7b4ccca12c0f58643f7cbaba696a68173f02101
Adrian <adrianasdfs@gmail.com>
2016-10-04 15:01:34 +0800

    add function to render rules

--- Assets/scripts/RulesContainer.cs
+++ Assets/scripts/RulesContainer.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Newtonsoft.Json.Linq;
 
 public class RulesContainer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public GameObject rulePrefab;
+
+	public void addRules(JArray rules){
+
+		// Render all the rules to the list
+
+		for (int i = 0; i < rules.Count; i++) {
+
+			JObject tk = JObject.Parse (rules.GetItem (i).ToString ());
+
+			GameObject ruleItem = Instantiate (rulePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+			ruleItem.GetComponent<RuleItem> ().addDetails (tk.GetValue ("name").ToString (), tk.GetValue ("description").ToString (), tk.GetValue ("ruleId").ToString ());
 	
+		}
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+
+
 }
