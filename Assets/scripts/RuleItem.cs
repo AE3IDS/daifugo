eb5d753117999e366fbaa8efd2753dbbfaec70ac
Adrian <adrianasdfs@gmail.com>
2016-10-04 14:46:20 +0800

    add id parameter to addDetails

--- Assets/scripts/RuleItem.cs
+++ Assets/scripts/RuleItem.cs
@@ -7,11 +7,13 @@ public class RuleItem : MonoBehaviour {
 	public GameObject ruleName;
 	public GameObject ruleDescription;
 
+	private string _ruleId;
 
-	public void addDetails(string name,string description){
+	public void addDetails(string name,string description,string id){
 	
 		ruleName.GetComponent<Text> ().text = name;
 		ruleDescription.GetComponent<Text> ().text = description;
+		_ruleId = id;
 
 	}
 
