8c519fc3779432b66326ed8391dffcdc57d1aa59
Adrian <adrianasdfs@gmail.com>
2016-10-04 22:51:05 +0800

    list of constants

---
+++ Assets/scripts/Constant.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Constant{
+
+	public const  int GREET_CODE = 101;
+	public const int NEWPLAYER_CODE = 102;
+	public const int MOVE_CODE = 103;
+	public const int ROOMLIST_CODE = 104;
+	public const int FETCHRULE_CODE = 105;
+	public const int SELECTEDRULE_CODE = 106;
+
+}
