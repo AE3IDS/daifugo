304819a59e9b9cf431c7491c09e8b9669e576e83
Adrian <adrianasdfs@gmail.com>
2016-10-09 01:49:33 +0800

    Object to test whether device has internet connectivity

---
+++ Assets/scripts/TestConnection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+
+public class TestConnection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+
+		try{
+
+			TcpClient testClient = new TcpClient ("192.168.2.1",3000);
+
+		}
+		catch(SocketException e){
+
+			Debug.Log ("error connecting");
+
+		}
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
