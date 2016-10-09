12c4befff75d9cc0b823f781ad93cfb96d65189b
Adrian <adrianasdfs@gmail.com>
2016-10-09 12:35:38 +0800

    move all game related script to folder

---
+++ Assets/scripts/Game/Game.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+using Newtonsoft.Json.Linq;
+
+public class Game : MonoBehaviour,SocketConnectionInterface {
+
+
+	// public variables
+
+	public GameObject gameTable, Overlay, round;
+
+
+	private Table _tableComponent;
+	private Text _roundTextComponent;
+	private OverlayScript _overlayscriptComponent;
+	private bool _hasReceivedData = false;
+	private int _responseCode;
+	private JObject _responseData;
+	private SocketConnection _socket;
+
+
+
+	void changeRound(string roundInt){
+		string[] g = _roundTextComponent.text.Split (' ');
+		_roundTextComponent.text = g [0] + " " + roundInt;
+	}
+
+
+	IEnumerator receiveDataCoroutine(){
+
+		while (true) {
+
+			while (!_hasReceivedData) {
+				yield return null;
+			}
+				
+
+			if (_responseCode == Constant.GREET_CODE) {
+
+				changeRound (_responseData ["round"].ToString ());
+
+				yield return new WaitForSeconds (0.6f);
+
+				_overlayscriptComponent.toggleLoadingText ();
+				_overlayscriptComponent.toggleWaitingText ();
+
+			} else if (_responseCode == Constant.NEWPLAYER_CODE) {
+
+//				Debug.Log ("new player");
+
+				_tableComponent.addUser (_responseData.GetValue ("userId").ToString(), _responseData.GetValue ("photoId").ToObject<int>());
+
+			} else if (_responseCode == Constant.GAMEROOM_OCCUPIED) {
+
+				Debug.Log ("occupied");
+
+				// hide overlay
+
+				_overlayscriptComponent.toggle();
+				_overlayscriptComponent.toggleWaitingText ();
+
+
+				//distribute cards
+
+			}
+
+
+			_hasReceivedData = false;
+		}
+
+	}
+
+	#region SocketConnectionInterface
+
+	public void receiveData(string dt){
+
+		JArray objects = JArray.Parse (dt);
+
+		JToken response = objects.First["response"];
+		_responseCode = (response ["code"]).ToObject<int>();
+		_responseData = JObject.Parse (response ["data"].ToString ());
+
+		_hasReceivedData = true;
+
+	}
+
+	public void handleError(){
+		Debug.Log ("game e");
+	}
+
+	#endregion
+
+	#region Unity stuff
+
+	void Start () {
+
+		// Initialize private variables
+
+		_roundTextComponent = round.GetComponent<Text> ();
+		_tableComponent = gameTable.GetComponent<Table> ();
+		_overlayscriptComponent = Overlay.GetComponent<OverlayScript> ();
+
+
+		// find socket from prev scene and send greet message
+
+		_socket = (GameObject.FindWithTag ("socket")).GetComponent<SocketConnection> ();
+		_socket.setDelegate (this);
+
+		Dictionary <string,object> obj = new Dictionary<string, object> {
+			{ "userId",PlayerPrefs.GetString ("user") }
+		};
+
+		_socket.greetServer (obj);
+
+
+		// show overlay and loading text
+
+		_overlayscriptComponent.toggle();
+		_overlayscriptComponent.toggleLoadingText ();
+	}
+
+	void Update () {
+
+
+	}
+
+	void Awake(){
+
+		StartCoroutine ("receiveDataCoroutine");
+	}
+
+	#endregion
+}
---
+++ Assets/scripts/Game/Table.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Table : MonoBehaviour {
+
+
+	private const string tablePlayerTag = "tablePlayer";
+	public GameObject overlay;
+	public GameObject userActions;
+	public GameObject user;
+	public GameObject cards;
+
+	public GameObject f;
+	private bool test =false;
+
+	// Use this for initialization
+
+
+
+	void Awake(){
+
+	}
+
+
+
+	void Start () {
+
+//		addUser("asdfs",1);
+//		addUser ("asdfs", 5);
+	}
+
+	public void distribute(){
+		f.GetComponent<Animator> ().SetBool ("distribute", true);
+	}
+
+	public void showCardUser(){
+
+
+		// hide all user action buttons
+		int childCount = userActions.transform.childCount;
+
+		 for (int m = 0; m < childCount; m++) {
+			GameObject child = userActions.transform.GetChild (m).gameObject;
+			child.GetComponent<Animator> ().SetBool ("showCard", true);
+		}
+
+
+		Invoke ("show", 1.3f);
+
+
+	}
+
+	private void show(){
+
+		overlay.SetActive (true);
+		overlay.GetComponent<OverlayScript> ().toggleCards ();
+	}
+
+	/*
+	 * 
+	 * add user Id to owner
+	 * 
+	 */
+
+	public void addOwnerId(string id){
+
+		user.GetComponent<UserTable> ().userId = id;
+
+	}
+
+
+
+	/*
+	 * 
+	 * Start Distribute the Cards
+	 * 
+	 */
+
+	public void distribute1(){
+
+	}
+
+		
+	/*
+	 * 
+	 * Get the first vacant seat on the table
+	 * 
+	 */ 
+
+	public void addUser(string userid, int photoId){
+
+		int childCount = gameObject.transform.childCount;
+		GameObject emptySpace = null;
+		UserTable t = null;
+
+		for(int i =0 ; i < childCount;i++){
+
+			GameObject childObject = gameObject.transform.GetChild (i).gameObject;
+			t = childObject.GetComponent<UserTable> ();
+
+			if (childObject.tag == tablePlayerTag && !t.spaceOccupied) {
+				
+				emptySpace = childObject;
+				break;
+
+			}
+		}
+
+		t.userId = userid;
+		t.addPhoto (photoId);
+		t.spaceOccupied = true;
+	}
+
+	// Update is called once per frame
+
+	void Update () {
+	
+	}
+}
