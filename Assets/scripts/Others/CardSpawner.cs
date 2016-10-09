5da18db99231b650bbfee1abb79141e1fdd88c46
Adrian <adrianasdfs@gmail.com>
2016-10-09 12:44:20 +0800

    other scripts

---
+++ Assets/scripts/Others/CardSpawner.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class CardSpawner{
+
+	private Dictionary<string,string> _clubSuit;
+	private Dictionary<string,string> _heartSuit;
+	private Dictionary<string,string> _diamondSuit;
+	private Dictionary<string,string> _spadesSuit;
+
+
+	public CardSpawner(){
+
+		_heartSuit = new Dictionary<string, string> ();
+		_clubSuit = new Dictionary<string, string> ();
+		_diamondSuit = new Dictionary<string, string> ();
+		_spadesSuit = new Dictionary<string, string> ();
+
+		string[] type = new string[] { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+		string[] r = new string[] { "_of_hearts", "_of_clubs", "_of_spades", "_of_diamonds" };
+
+		for (int i = 0; i < r.Length; i++) {	
+			for (int j = 0; j < type.Length; j++) {
+				
+				Dictionary<string,string> g = getDictionaryForKey (i);
+
+				if (g != null) {
+					g.Add (type [j],type[j].ToLower()+r[i] );
+				}
+			}
+		}
+
+	}
+
+	public GameObject produceCard(string type, string value1, float width,float height){
+
+		string imageName = null;
+		GameObject card = new GameObject();
+
+		Dictionary <string,string> g = getDictionaryForKey (Int32.Parse (type));
+
+		if (g != null) {
+			g.TryGetValue(value1, out imageName);
+		}
+
+		// Add Component to the card GameOBject
+
+		RectTransform r = card.AddComponent<RectTransform> ();
+
+		r.sizeDelta = new Vector2 (width, height);
+
+		Image img = card.AddComponent<Image>();
+
+		Texture2D tex = Resources.Load ("images/cards/"+ getFolderNameForKey(Int32.Parse(type)) + "/" + imageName, typeof(Texture2D)) as Texture2D;
+		img.sprite = Sprite.Create (tex, new Rect (0, 0,tex.width, tex.height), new Vector2 (0.0f, 0.0f));
+
+		return card;
+	}
+
+	private Dictionary<string,string> getDictionaryForKey(int key){
+
+		if (key == 0) {
+			return _heartSuit;
+		} else if (key == 1) {
+			return _clubSuit;
+		} else if (key == 2) {
+			return _diamondSuit;
+		} else if (key == 3) {
+			return _spadesSuit;
+		}
+
+		return null;
+
+	}
+
+	private string getFolderNameForKey(int key){
+
+		if (key == 0) {
+			return "hearts";
+		} else if (key == 1) {
+			return "club";
+		} else if (key == 2) {
+			return "diamonds";
+		} else if (key == 3) {
+			return "spades";
+		}
+
+		return null;
+
+	}
+
+
+}
---
+++ Assets/scripts/Others/SocketConnection.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using WebSocketSharp;
+using Newtonsoft.Json;
+
+public class SocketConnection : MonoBehaviour{
+
+
+	/*
+	 * 
+	 * Code 101: greet server
+	 * Code 102: new player
+	 * Code 103: move
+	 * Code 104: room list
+	 * Code 105: fetch rule list
+	 * Code 106: selected rule list
+	 * 
+	 * 
+	 */
+
+	private SocketConnectionInterface _delegator;
+	private WebSocket _sock;
+	List<string> requestPool = new List<string> ();
+
+	#region class definition
+
+	IEnumerator sendData(){
+
+		while (true) {
+
+			while((_sock == null) ||  (!_sock.IsAlive || requestPool.Count == 0)) {
+				yield return null;
+			}
+				
+			foreach (string value in requestPool) {
+				_sock.SendAsync (value, null);
+			}
+
+			requestPool.Clear ();
+
+		}
+	}
+
+	void Start(){
+
+		Debug.Log ("start connecting");
+
+		_sock = new WebSocket ("ws://192.168.2.1:3000",new string[] { "echo-protocol" });
+
+		//OnMessage event handler
+
+		_sock.OnMessage += (sender, e) => {
+
+			Debug.Log("receive message");
+
+			if(e.IsText){
+				Debug.Log(e.Data);
+				(this._delegator).receiveData(e.Data);
+			}
+
+		};
+
+		// OnError event handler
+
+		_sock.OnError += (sender, e) => {
+			Debug.Log("error occured");
+			Debug.Log(e.Message);
+		};
+
+		_sock.ConnectAsync ();
+
+
+		StartCoroutine ("sendData");
+
+	}
+
+	#endregion
+
+	public void setDelegate(SocketConnectionInterface i){
+		this._delegator = i;
+	}
+
+
+	public void sendLobbyDetails(Dictionary<string,object> dt){
+
+		Debug.Log (JSONMaker.makeJSON (Constant.LOBBYDETAILS_CODE, dt));
+		requestPool.Add (JSONMaker.makeJSON (Constant.LOBBYDETAILS_CODE,dt));
+	}
+		
+	public void greetServer(Dictionary<string,object> dt){
+		requestPool.Add (JSONMaker.makeJSON(Constant.GREET_CODE,dt));
+	}
+
+	public void fetchRules(){
+		requestPool.Add (JSONMaker.makeJSON(Constant.FETCHRULE_CODE));
+	}
+
+	public void getRoom(){
+		requestPool.Add (JSONMaker.makeJSON(Constant.ROOMLIST_CODE));
+	}
+		
+}
