fbf01f7e53ebbccdb86fecd8dbc465be9d1af758
Adrian <adrianasdfs@gmail.com>
2016-10-10 19:38:27 +0800

    change the logic of checking for internet connectivity

--- Assets/scripts/Lobby/TestConnection.cs
+++ Assets/scripts/Lobby/TestConnection.cs
@@ -9,34 +9,18 @@ public class TestConnection {
 	private int _port;
 
 	private TestConnectionInterface _delegate;
-	private TcpClient _s;
-
-	public TestConnection(string url, int port){
-
-		this._ip = url;
-		this._port = port;
-		_s = new TcpClient ();
-	}
 
 	public void setDelegate(TestConnectionInterface i){
 		this._delegate = i;
 	}
 
 	public void startTest(){
-		_s.BeginConnect(this._ip,this._port,new AsyncCallback(AcceptCallback),_s);
-	}
 
-	public  void AcceptCallback(IAsyncResult ar) {
-
-		try{
-			_s.EndConnect(ar);
-			this._delegate.giveStatus(true);
-		}
-		catch(SocketException e){
-			Debug.Log ("done " + e.ErrorCode.ToString() );
-			this._delegate.giveStatus (false);
-		}
+		bool hasNoInternet = Application.internetReachability == NetworkReachability.NotReachable;
+		this._delegate.giveStatus (!hasNoInternet);
 
 	}
+
+
 
 }
