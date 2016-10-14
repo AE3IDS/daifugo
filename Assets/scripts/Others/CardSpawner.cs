0f0d6f2949f7703c2242952231f3fb816d3d7743
Adrian <adrianasdfs@gmail.com>
2016-10-15 00:14:44 +0800

    remove produceCard function and refactor class

--- Assets/scripts/Others/CardSpawner.cs
+++ Assets/scripts/Others/CardSpawner.cs
@@ -6,61 +6,51 @@ using UnityEngine.UI;
 
 public class CardSpawner{
 
-	private Dictionary<string,string> _clubSuit;
-	private Dictionary<string,string> _heartSuit;
-	private Dictionary<string,string> _diamondSuit;
-	private Dictionary<string,string> _spadesSuit;
+	private Dictionary<int,string> _clubSuit;
+	private Dictionary<int,string> _heartSuit;
+	private Dictionary<int,string> _diamondSuit;
+	private Dictionary<int,string> _spadesSuit;
 
 
 	public CardSpawner(){
 
-		_heartSuit = new Dictionary<string, string> ();
-		_clubSuit = new Dictionary<string, string> ();
-		_diamondSuit = new Dictionary<string, string> ();
-		_spadesSuit = new Dictionary<string, string> ();
+		_heartSuit = new Dictionary<int, string> ();
+		_clubSuit = new Dictionary<int, string> ();
+		_diamondSuit = new Dictionary<int, string> ();
+		_spadesSuit = new Dictionary<int, string> ();
 
-		string[] type = new string[] { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+	
 		string[] r = new string[] { "_of_hearts", "_of_clubs", "_of_spades", "_of_diamonds" };
 
 		for (int i = 0; i < r.Length; i++) {	
-			for (int j = 0; j < type.Length; j++) {
+			for (int j = 2; j < 16; j++) {
 				
-				Dictionary<string,string> g = getDictionaryForKey (i);
+				Dictionary<int,string> g = getDictionaryForKey (i);
+				g.Add (j,j.ToString()+r[i] );
 
-				if (g != null) {
-					g.Add (type [j],type[j].ToLower()+r[i] );
-				}
 			}
 		}
 
 	}
 
-	public GameObject produceCard(string type, string value1, float width,float height){
+	public Sprite getSprite (int suit, int rank){
 
 		string imageName = null;
-		GameObject card = new GameObject();
-
-		Dictionary <string,string> g = getDictionaryForKey (Int32.Parse (type));
+		Dictionary <int,string> g = getDictionaryForKey (suit);
 
 		if (g != null) {
-			g.TryGetValue(value1, out imageName);
+			g.TryGetValue(rank, out imageName);
 		}
+			
+		Texture2D tex = Resources.Load ("images/cards/"+ getFolderNameForKey(suit) + "/" + imageName, typeof(Texture2D)) as Texture2D;
+		Sprite x = Sprite.Create (tex, new Rect (0, 0,tex.width, tex.height), new Vector2 (0.0f, 0.0f));
 
-		// Add Component to the card GameOBject
+		return x;
 
-		RectTransform r = card.AddComponent<RectTransform> ();
-
-		r.sizeDelta = new Vector2 (width, height);
-
-		Image img = card.AddComponent<Image>();
-
-		Texture2D tex = Resources.Load ("images/cards/"+ getFolderNameForKey(Int32.Parse(type)) + "/" + imageName, typeof(Texture2D)) as Texture2D;
-		img.sprite = Sprite.Create (tex, new Rect (0, 0,tex.width, tex.height), new Vector2 (0.0f, 0.0f));
-
-		return card;
 	}
 
-	private Dictionary<string,string> getDictionaryForKey(int key){
+
+	private Dictionary<int,string> getDictionaryForKey(int key){
 
 		if (key == 0) {
 			return _heartSuit;
