bbca5d4ce77b8a02d8dde248445c2de0856818b7
Adrian <adrianasdfs@gmail.com>
2016-11-30 23:59:57 +0800

    script for the card

---
+++ Assets/scripts/Game/CardScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CardScript : MonoBehaviour {
+
+	int _index;
+	public int cardIndex{
+
+		get {
+			return this._index;
+		}
+
+		set{
+			this._index = value;
+		}
+
+	}
+
+	bool _isClicked = false;
+	public bool cardClicked{
+
+		get{
+			return this._isClicked;
+		}
+
+		set{
+
+			this._isClicked = value;
+		}
+
+	}
+
+
+
+
+	int _suit;
+	public int cardSuit	{
+
+		get{
+
+			return this._suit;
+		}
+	}
+		
+	int _rank;
+	public int cardRank {
+
+		get {
+
+			return this._rank;
+
+		}
+	}
+
+	public void setCardDetails(int suit, int rank){
+
+		this._suit = suit;
+		this._rank = rank;
+
+		CardSpawner s = new CardSpawner ();
+		gameObject.GetComponent<Image>().sprite = s.getSprite (suit, rank);
+
+	}
+
+}
