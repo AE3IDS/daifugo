b6a5463a57a04e7269cd028a9fb4193b87f8132c
Adrian <adrianasdfs@gmail.com>
2016-12-02 20:18:14 +0800

    change implementation of the test

--- Assets/Editor/JSONMakerTest.cs
+++ Assets/Editor/JSONMakerTest.cs
@@ -7,33 +7,38 @@ using System.Collections.Generic;
 public class JSONMakerTest {
 
 	[Test]
-	public void testMakeJSONArray()
-	{
-		int sampleCode = 110;
+	public void testMakeJSONArray(){
+		
 		int numOfSelectedCards = 3;
 
-		List<Dictionary<string,int>[]> s = new List<Dictionary<string, int>[]> ();
+		List<Dictionary<string,int>> s = new List<Dictionary<string, int>> ();
 		int randomSuit = -1;
 		int randomKind = -1;
 
-		string expectedOutput = "{\"code\":" + sampleCode + ",\"data\":[";
+		string expectedOutput = "{\"code\":" + Constant.DEALCARD_CODE + ",\"userId\":\"ASDFS\"" + ",\"data\":[";
+
+
+		/* initialize heading of json */
+
+		Dictionary<string,object> heading = new Dictionary<string, object> {
+			{"code",Constant.DEALCARD_CODE},
+			{"userId","ASDFS"}
+		};
+
+
+		/* initialize main data of json */
 
 		for (int i = 0; i < numOfSelectedCards; i++) {
-			
-			Dictionary<string,int>[] cardItem = new Dictionary<string, int> [2];
 
 			randomSuit = (int)Random.Range (1f, 4f);
-			Dictionary<string,int> cardSuit = new Dictionary<string,int> ();
-			cardSuit.Add ("suit",randomSuit);
-
 			randomKind = (int)Random.Range (1f, 15f);
-			Dictionary<string,int> cardRank = new Dictionary<string,int> ();
-			cardRank.Add ("kind",randomKind);
 
-			cardItem [0] = cardSuit;
-			cardItem [1] = cardRank;
+			Dictionary<string,int> cardDetails = new Dictionary<string,int> {
+				{ "suit",randomSuit },
+				{ "kind",randomKind }
+			};
 
-			s.Add (cardItem);
+			s.Add (cardDetails);
 			string m = "{\"suit\":" + randomSuit + ",\"kind\":" + randomKind + "}";
 			m += (i != 3 - 1) ? "," : "";
 			expectedOutput += m;
@@ -42,7 +47,7 @@ public class JSONMakerTest {
 		expectedOutput += "]}";
 
 
-		string outputJSON = JSONMaker.makeJSONArray (sampleCode, s.ToArray());
+		string outputJSON = JSONMaker.makeJSONArray (heading, s.ToArray());
 		outputJSON = outputJSON.Replace ("\n", "");
 		outputJSON = outputJSON.Replace (" ", "");
 
