4159e0f3c60682e862db92e6b8ab7a6bf1b84cf0
Adrian <adrianasdfs@gmail.com>
2016-12-02 18:06:34 +0800

    add DEALCARD code

--- Assets/scripts/Others/Constant.cs
+++ Assets/scripts/Others/Constant.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public static class Constant{
 
-	public const  int GREET_CODE = 101;
+	public const int GREET_CODE = 101;
 	public const int NEWPLAYER_CODE = 102;
 	public const int MOVE_CODE = 103;
 	public const int ROOMLIST_CODE = 104;
@@ -14,6 +14,7 @@ public static class Constant{
 	public const int REQUESTCARD_CODE = 109;
 	public const int READY_CODE = 110;
 	public const int STARTGAME_CODE = 111;
+	public const int DEALCARD_CODE = 112;
 
 	public const int GAMEROOM_OCCUPIED = 50;
 
