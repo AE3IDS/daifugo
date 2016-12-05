e5ddd35fe57e2959104642f3361b2d0f593679fb
Adrian <adrianasdfs@gmail.com>
2016-12-05 23:41:03 +0800

    move the spawning to a coroutine

--- Assets/DealtCardsRenderer.cs
+++ Assets/DealtCardsRenderer.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DealtCardsRenderer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	private const float cardSpace = 45.0f;
+	private Vector2 minCardAnchor = new Vector2 (0.5f, 0.5f);
+	private Vector2 maxCardAnchor = new Vector2(0.5f,0.5f);
+
+	public GameObject card;
+
+	IEnumerator addCoroutine(float startX, int[][] cards){
+
+		for (int i = 0; i < cards.Length; i++) {
+
+			GameObject j = (GameObject) Instantiate (card, new Vector3 (startX, 0, 0), Quaternion.identity);
+
+			RectTransform cardRect = j.GetComponent<RectTransform> ();
+
+			j.transform.SetParent (gameObject.transform,true);
+			j.GetComponent<Button> ().interactable = false;
+
+
+			/* set size of the card */
+
+			float containerHeight = gameObject.GetComponent<RectTransform> ().sizeDelta.y;
+			cardRect.sizeDelta = new Vector2 (74.0f, containerHeight);
+
+
+			j.GetComponent<CardScript> ().setCardDetails (cards[i][0], cards[i][1]);
+			cardRect.anchorMin = minCardAnchor;
+			cardRect.anchorMax = maxCardAnchor;
+
+			cardRect.anchoredPosition3D = new Vector3 (startX, 0, 0);
+			startX += cardSpace;
+
+			yield return new WaitForSeconds (0.75f);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+
+	public void displayCards(int[][] cards){
+
+		float startX = 0.0f;
+
+		if (cards.Length % 2 != 0) {
+			int numOfLeftCards = cards.Length / 2;
+			startX = numOfLeftCards * cardSpace * -1;
+		}
+			
+		StartCoroutine (addCoroutine (startX, cards));
 	}
 }
