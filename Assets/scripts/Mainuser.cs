f48cd3dca942a2e57121565713f4e2e69b00f796
Adrian <adrianasdfs@gmail.com>
2016-12-07 01:10:58 +0800

    refactor addCard function

--- Assets/scripts/Mainuser.cs
+++ Assets/scripts/Mainuser.cs
@@ -7,6 +7,11 @@ using System;
 public class Mainuser : UserTable {
 
 	public float CARD_SPACE = 100.0f;
+
+	private const float cardSpace = 45.0f;
+	private Vector2 minCardAnchor = new Vector2 (0.5f, 0.5f);
+	private Vector2 maxCardAnchor = new Vector2(0.5f,0.5f);
+
 	public float CARD_Y = 22.0f;
 	public float cardXMain = 87.0f;
 	public GameObject card;
@@ -17,6 +22,8 @@ public class Mainuser : UserTable {
 	private List<GameObject> cards;
 	private bool _turn = false;
 
+	public GameObject space;
+
 	void Start(){
 
 		cards = new List<GameObject> ();
@@ -25,31 +32,35 @@ public class Mainuser : UserTable {
 	}
 
 
+	void cardHandler(GameObject j){
+
+		j.GetComponent<CardScript>().cardClicked = !j.GetComponent<CardScript>().cardClicked;
+		
+		bool isClicked = j.GetComponent<CardScript>().cardClicked;
+		j.GetComponent<Animator> ().SetBool ("clicked", isClicked);
+
+		if(isClicked){
+			selectedCards.Add(j);
+		}else{
+			selectedCards.Remove(j);
+		}
+
+	}
+
 	public void addCards(int suit, int rank){
 
-		GameObject j = Instantiate (card, new Vector3(cardXMain,CARD_Y,0), Quaternion.identity) as GameObject;
 
-		j.GetComponent<CardScript> ().setCardDetails (suit, rank);
-		j.GetComponent<Button> ().interactable = false;
-		j.transform.SetParent (transform,true);
-		j.GetComponent<RectTransform> ().anchoredPosition3D = new Vector3 (cardXMain, CARD_Y, 0);
+		CardMaker card = new CardMaker ();
 
-		j.GetComponent<Button> ().onClick.AddListener (delegate {
+		card.setCardDetails (suit, rank);
+		card.setCardInteractable (false);
+		card.getCard ().transform.SetParent (transform, true);
+		card.set3DPosition(new Vector3 (cardXMain, CARD_Y, 0));
+		card.setCardSize(new Vector2(168.0f,250.0f));
 
-			j.GetComponent<CardScript>().cardClicked = !j.GetComponent<CardScript>().cardClicked;
-
-			bool isClicked = j.GetComponent<CardScript>().cardClicked;
-			j.GetComponent<Animator> ().SetBool ("clicked", isClicked);
-
-			if(isClicked){
-				selectedCards.Add(j);
-			}else{
-				selectedCards.Remove(j);
-			}
-
-		});
-			
-		cards.Add (j);
+		card.addHandler (delegate { cardHandler (card.getCard ()); });
+	
+		cards.Add (card.getCard());
 		cardXMain += CARD_SPACE;
 
 	}
@@ -59,6 +70,9 @@ public class Mainuser : UserTable {
 	{
 		Destroy (c.gameObject);
 	}
+
+
+
 
 
 	public void toggleTurn(){
@@ -73,20 +87,62 @@ public class Mainuser : UserTable {
 
 	}
 
-	public void endDealt(){
+	IEnumerator addCoroutine(float startX, int[][] cards){
 
-		action.SetActive (false);
+		yield return new WaitForSeconds (0.6f);
 
-		for (int i =0;i < transform.childCount;i++) {
-			
-			GameObject child = transform.GetChild (i).gameObject;
-			if (selectedCards.Contains (child)) {
-				child.SetActive (false);
-			}
-	
+		GameObject[] selectedCardsAr = selectedCards.ToArray ();
+
+		for (int i = 0; i < cards.Length; i++) {
+
+			selectedCards [i].GetComponent<Animator> ().SetBool ("dealt", true);
+			yield return new WaitForSeconds (0.5f);
+//			selectedCards [i].SetActive (false);
+
+
+			GameObject j = (GameObject) Instantiate (card, new Vector3 (startX, 0, 0), Quaternion.identity);
+
+			RectTransform cardRect = j.GetComponent<RectTransform> ();
+
+			j.transform.SetParent (space.transform,true);
+			j.GetComponent<Button> ().interactable = false;
+
+
+			/* set size of the card */
+
+			float containerHeight = space.GetComponent<RectTransform> ().sizeDelta.y;
+			cardRect.sizeDelta = new Vector2 (74.0f, containerHeight);
+
+
+			j.GetComponent<CardScript> ().setCardDetails (cards[i][0], cards[i][1]);
+			cardRect.anchorMin = minCardAnchor;
+			cardRect.anchorMax = maxCardAnchor;
+
+			cardRect.anchoredPosition3D = new Vector3 (startX, 0, 0);
+			startX += cardSpace;
+
+			yield return new WaitForSeconds (0.9f);
 		}
 
 		selectedCards.Clear ();
+		yield return null;
+	}
+
+
+	public void endDealt(int[][] cards){
+
+		action.SetActive (false);
+
+		float startX = 0.0f;
+
+		if (cards.Length % 2 != 0) {
+			int numOfLeftCards = cards.Length / 2;
+			startX = numOfLeftCards * cardSpace * -1;
+		}
+
+		StartCoroutine (addCoroutine (startX, cards));
+
+
 
 	}
 		
