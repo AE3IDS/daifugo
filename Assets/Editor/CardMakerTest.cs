bdae7fd6da5c8587a7a0d869ea4ec190fb605b05
Adrian <adrianasdfs@gmail.com>
2016-12-07 01:13:32 +0800

    test file for CardMaker

---
+++ Assets/Editor/CardMakerTest.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using UnityEngine.UI;
+
+[TestFixture]
+public class CardMakerTest {
+
+	[Test]
+	public void CardMaker_Constructor_InitializeCard()
+	{
+		CardMaker cardM = new CardMaker ();
+		Assert.NotNull (cardM.getCard ());
+	}
+
+	[Test]
+	public void Test_setCardInteractable(){
+
+		CardMaker cardM = new CardMaker ();
+		cardM.setCardInteractable (false);
+
+		GameObject card = cardM.getCard ();
+		Assert.IsTrue(card.GetComponent<Button> ().interactable == false);
+
+	}
+
+	[Test]
+	public void Test_set3DPosition(){
+
+		Vector3 pos = new Vector3 (2.0f, 3.0f, 4.0f);
+
+		CardMaker cardM = new CardMaker ();
+		cardM.set3DPosition (pos);
+
+		GameObject card = cardM.getCard ();
+		Assert.IsTrue(card.GetComponent<RectTransform>().anchoredPosition3D.Equals(pos));
+
+	}
+
+
+	[Test]
+	public void Test_setCardDetails(){
+
+		int randomSuit = (int)Random.Range (1f, 4f);
+		int randomKind = (int)Random.Range (3f, 15f);
+
+		CardMaker cardM = new CardMaker ();
+		cardM.setCardDetails (randomSuit, randomKind);
+
+		GameObject card = cardM.getCard ();
+		Assert.IsTrue(card.GetComponent<CardScript>().cardSuit == randomSuit);
+		Assert.IsTrue (card.GetComponent<CardScript> ().cardRank == randomKind);
+
+	}
+
+
+
+
+	[Test]
+	public void Test_setCardSize(){
+
+		Vector2 size = new Vector2 (2.0f, 3.0f);
+
+		CardMaker cardM = new CardMaker ();
+		cardM.setCardSize (size);
+
+		GameObject card = cardM.getCard ();
+		Assert.IsTrue(card.GetComponent<RectTransform>().sizeDelta == size);
+	
+	}
+
+
+	[Test]
+	public void Test_setCardParent(){
+
+		GameObject j = new GameObject ();
+
+		CardMaker cardM = new CardMaker ();
+		cardM.setCardParent (j);
+
+		Assert.IsTrue(cardM.getCard ().transform.IsChildOf (j.transform));
+		Assert.IsTrue (j.transform.childCount == 1);
+
+	}
+
+
+}
