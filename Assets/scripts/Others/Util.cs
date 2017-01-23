5b9c8a92ddb91c711afbe1b09ba8bd37e0640a90
Adrian <adrianasdfs@gmail.com>
2017-01-24 01:02:32 +0800

    utility class

---
+++ Assets/scripts/Others/Util.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Util{
+
+	public static Sprite getAvatarForId(int photoId){
+
+		string imageName = null;
+
+		if (photoId == 0) {
+
+			imageName = "dino";
+
+		} else if (photoId == 1) {
+
+			imageName = "minion";
+
+		} else if (photoId == 2) {
+
+			imageName = "dory";
+
+		} else if (photoId == 3) {
+
+			imageName = "monster";
+
+		} else if (photoId == 4) {
+
+			imageName = "anger";
+
+		} else if (photoId == 5) {
+
+			imageName = "baymax";
+
+		} else if (photoId == 6) {
+
+			imageName = "dory";
+
+		}
+
+		Texture2D avaTex = Resources.Load ("images/avatar/"+imageName, typeof(Texture2D)) as Texture2D;
+
+		return Sprite.Create (avaTex, new Rect (0, 0, avaTex.width, avaTex.height), new Vector2 (0.0f, 0.0f));
+
+	}
+
+}
