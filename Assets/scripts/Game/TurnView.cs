51093f90f934a6c452b371eca8897845fb4151fe
Adrian <adrianasdfs@gmail.com>
2017-01-24 01:01:53 +0800

    script file for turn interface

---
+++ Assets/scripts/Game/TurnView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TurnView : MonoBehaviour {
+
+	public GameObject ava;
+
+	public void setAvaTurnImage(int photoId){
+
+		ava.GetComponent<Image> ().sprite = Util.getAvatarForId (photoId);
+
+	}
+
+
+}
