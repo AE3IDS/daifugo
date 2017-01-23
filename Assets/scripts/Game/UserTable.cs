1acf71a5e83c8d9d5747fd4a016dc9610bed5491
Adrian <adrianasdfs@gmail.com>
2017-01-24 00:59:15 +0800

    refactor class

--- Assets/scripts/Game/UserTable.cs
+++ Assets/scripts/Game/UserTable.cs
@@ -15,6 +15,7 @@ public class UserTable : MonoBehaviour {
 	protected readonly float DISPLAYDEALTCARD_SPACE = 45.0f;
 	protected float cardX = 0;
 	protected readonly float CARDSPACE = 74.0f;
+	protected bool _turn = false;
 
 	protected readonly Vector2 minDealtCardAnchor = new Vector2 (0.5f, 0.5f);
 	protected readonly Vector2 maxDealtCardAnchor = new Vector2(0.5f,0.5f);
@@ -24,81 +25,56 @@ public class UserTable : MonoBehaviour {
 
 
 	private const float CARD_Y = 21.0f;
-	private bool turn = false;
 
 	private string _userId;
-	private int numOfCards = 0;
-	public string userId { get { return this._userId; } set { this._userId = value; } }
+
+	private int _numOfCards = 0;
+	public string userId { 
+		get { return this._userId; } 
+		set { this._userId = value; } 
+	}
 
 	private bool _isOccupied = false;
-	public bool spaceOccupied { get { return this._isOccupied; } set {this._isOccupied = value; }}
+
+	public bool spaceOccupied { 
+		get { return this._isOccupied; } 
+		set {this._isOccupied = value; }
+	}
 
 
 	/*
 	 * 
-	 * spawn the correct avatar based on the photoId
+	 * add avatar
 	 * 
 	 */ 
 
 	public void addPhoto(int photoId){
-
-		string imageName = null;
-
-		if (photoId == 0) {
-			
-			imageName = "dino";
-
-		} else if (photoId == 1) {
-
-			imageName = "minion";
-
-		} else if (photoId == 2) {
-
-			imageName = "dory";
-
-		} else if (photoId == 3) {
-
-			imageName = "monster";
-
-		} else if (photoId == 4) {
-
-			imageName = "anger";
-
-		} else if (photoId == 5) {
-
-			imageName = "baymax";
-
-		} else if (photoId == 6) {
-
-			imageName = "dory";
-
-		}
-			
-		Texture2D avaTex = Resources.Load ("images/avatar/"+imageName, typeof(Texture2D)) as Texture2D;
-
-		avatar.GetComponent<Image> ().sprite = Sprite.Create (avaTex, new Rect (0, 0, avaTex.width, avaTex.height), new Vector2 (0.0f, 0.0f));
-
+		
+		avatar.GetComponent<Image> ().sprite = Util.getAvatarForId (photoId);
 
 	}
 
+
 	private void addCards(){
 
-		numOfCards++;
+		_numOfCards++;
 
 		Text t = numOfCardsLabel.GetComponent<Text> ();
 
 		char[] space = new char[]{ '-',' ' };
 		string[] s = t.text.Split (space, 2);
 
-		t.text = numOfCards.ToString () + " " + s [1];
+		t.text = _numOfCards.ToString () + " " + s [1];
 
 	}
+
 
 	public virtual void endDealt(int[][] cards){
 
 
 
 	}
+
 
 	public void calculateX(int length){
 
@@ -119,15 +95,24 @@ public class UserTable : MonoBehaviour {
 	}
 
 	void OnCollisionEnter2D(Collision2D c){
+		
 		Destroy (c.gameObject);
 		addCards ();
 
 	}
 
+
+	public bool getTurn(){
+
+		return this._turn;
+
+	}
+
+
 	public void toggleTurn(){
 
-		turn = !turn;
-		turnArrow.SetActive (turn);
+		_turn = !_turn;
+//		turnArrow.SetActive (_turn);
 
 	}
 
