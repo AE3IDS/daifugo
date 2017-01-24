95af323ef18340b61af7eb7b3e5841f34cd187cd
Adrian <adrianasdfs@gmail.com>
2017-01-24 14:57:11 +0800

    script file room list items

---
+++ Assets/scripts/Game/RoomItem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class RoomItem : MonoBehaviour {
+
+	public GameObject roomName;
+	public GameObject rules;
+	public GameObject numOfPlayers;
+
+
+	public void setItemDetails(){
+
+
+		/* set rules test */
+
+		Text txtRules = rules.GetComponent<Text> ();
+
+
+		/* set numOfPlayers */
+
+		Text txtNumOfPlayers  = numOfPlayers.GetComponent<Text>();
+
+
+		/* set room name */
+
+		Text txtName = roomName.GetComponent<Text> ();
+
+
+	}
+
+
+	public void joinButtonHandler(){
+
+
+
+	}
+
+
+}
