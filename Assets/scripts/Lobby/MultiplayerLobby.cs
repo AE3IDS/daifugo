89526c0ad7513755303ff19b4474917f02b84747
Adrian <adrianasdfs@gmail.com>
2017-01-29 01:03:24 +0800

    implement the SocketConnectionInterface

--- Assets/scripts/Lobby/MultiplayerLobby.cs
+++ Assets/scripts/Lobby/MultiplayerLobby.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MultiplayerLobby : MonoBehaviour {
+public class MultiplayerLobby : MonoBehaviour,SocketConnectionInterface {
 
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
+	public void receiveData(string dt)
+	{
+		JArray objects = JArray.Parse (dt);
+
+		JToken response = objects.First["response"];
+		_responseTk = response.SelectToken ("data");
+		_responseCd = (int)response.SelectToken("code");
+	}
+
+
+	public void handleError()
+	{
+
+	}
+
+
 	// Update is called once per frame
 	void Update () {
 	
