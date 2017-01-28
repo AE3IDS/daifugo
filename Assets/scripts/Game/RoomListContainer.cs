c90e79aff0cceb481c8debb8b6b55d5fa0bf6f00
Adrian <adrianasdfs@gmail.com>
2017-01-28 11:10:40 +0800

    add addRooms function

--- Assets/scripts/Game/RoomListContainer.cs
+++ Assets/scripts/Game/RoomListContainer.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RoomListContainer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+
+	public void addRooms(Dictionary<string,string> roomDetails)
+	{
+
+		GameObject room = Resources.Load ("prefabs/roomItem", typeof(GameObject)) as GameObject;
+		GameObject newRoom = Instantiate (room, Vector3.zero, Quaternion.identity, transform) as GameObject;
+
+		string roomName = null;
+		string numOfPlayer = null;
+		string rules = null;
+
+		roomDetails.TryGetValue ("roomId", out roomName);
+		roomDetails.TryGetValue ("numOfPlayers", out numOfPlayer);
+		roomDetails.TryGetValue ("rules", out rules);
+
+		newRoom.GetComponent<RoomItem> ().setItemDetails (rules, roomName, numOfPlayer);
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
